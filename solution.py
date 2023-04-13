3b1e5ed2a65a70d3e878930e2dc00bf95deea0cf
Anna Golubeva <110536476+angolechka@users.noreply.github.com>
2023-04-13 17:47:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,5 +1,7 @@
-import pandas as pd
+from scipy import stats as st
 import numpy as np
+import math as mth
+from statsmodels.stats.proportion import proportions_ztest
 
 
 chat_id = 266642884 # Ваш chat ID, не меняйте название переменной
